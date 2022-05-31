ba7a4ec0cdded7128c706716152161b450658d43
BladedShip <67158370+BladedShip@users.noreply.github.com>
2022-05-31 21:37:15 +0530

    Update 0.01
    +jsconfig.json
    +updated script.js
    +css optimisations
    +updated Footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
-window.onload = function(){
+function loadexec(){
     var element = document.getElementById('flex'),
     style = window.getComputedStyle(element),
     top = style.getPropertyValue('height');
     console.log(top);
     console.log("Made by Adithyan, lol");
 }
+console.log('Hello World');

b276b1e6a2f9ef92f3812501115621cb4f6b065a
BladedShip <67158370+BladedShip@users.noreply.github.com>
2022-05-31 22:09:26 +0530

    Update 0.01v2 Code Formatted

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
-function loadexec(){
-    var element = document.getElementById('flex'),
+function loadexec() {
+  var element = document.getElementById("flex"),
     style = window.getComputedStyle(element),
-    top = style.getPropertyValue('height');
-    console.log(top);
-    console.log("Made by Adithyan, lol");
+    top = style.getPropertyValue("height");
+  console.log(top);
+  console.log("Made by Adithyan, lol");
 }
-console.log('Hello World');
+console.log("Hello World");

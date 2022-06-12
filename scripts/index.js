699042b21c87b528ce20277670ec95fc7c4ecec1
BladedShip <67158370+BladedShip@users.noreply.github.com>
2022-06-12 14:18:42 +0530

    I hath almost given up-eth

--- scripts/index.js
+++
@@ -1,8 +0,0 @@
-function loadexec() {
-  var element = document.getElementById("flex"),
-    style = window.getComputedStyle(element),
-    top = style.getPropertyValue("height");
-  console.log(top);
-  console.log("Made by Adithyan, lol");
-}
-console.log("Hello World");
